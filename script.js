a9cb4d3154d4a9f64e629f3d108b68e0a232faa9
Mohadese Asri <silentsongmea39@yahoo.com>
2023-07-20 21:36:21 +0430

    type codes if and else for choose User and Computer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const choices=["rock","paper","scissors"];
-const userchoice=prompt("choose rock, paper, scissors :");
+const userchoices=prompt("choose rock, paper, scissors :");
+const userchoice=userchoices.toLowerCase();
 if(userchoice){
     console.log(`you choose: ${userchoice}`);
 }
@@ -9,3 +10,31 @@ else{
 const randomNumber=Math.floor(Math.random() *3);
 const computerChoice=choices[randomNumber];
 console.log(`Computer chooses:${computerChoice}`);
+
+if(userchoice === computerChoice){
+    console.log("It's a tie🙅‍♂️🙅‍♀️");
+}
+else if(userchoice ==="rock" ){
+    if(computerChoice === "scissors"){
+        console.log(" You win😜❤");
+    }
+    else{
+        console.log("You lose😂💔");
+    }
+}
+else if(userchoice === "paper"){
+    if(computerChoice === "rock"){
+        console.log("You win😜❤");
+    }
+    else{
+        console.log("You lose😂💔");
+    }
+}
+else if(userchoice ==="scissors"){
+    if(computerChoice === "paper"){
+        console.log("You win😜❤");
+    }
+    else{
+        console.log("You lose😂💔");
+    }
+}
